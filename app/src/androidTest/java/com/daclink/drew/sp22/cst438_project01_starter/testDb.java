e68284bcf8fa0bb609cf41a6728f1ca1d2f094e5
IGramajoO <igramajo@outlook.com>
2022-02-03 00:27:56 -0800

    Finished basic tests for UserDao in testDb.java, all tests are being passed

--- app/src/androidTest/java/com/daclink/drew/sp22/cst438_project01_starter/testDb.java
+++ app/src/androidTest/java/com/daclink/drew/sp22/cst438_project01_starter/testDb.java
@@ -2,6 +2,8 @@ package com.daclink.drew.sp22.cst438_project01_starter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import android.content.Context;
 
@@ -19,41 +21,34 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class testDb {
 
-    public static final String TAG = "Junit";
-//    private AppDatabase testDB;
-    private UserDao mUserDao;
+//    public static final String TAG = "Junit";
 
-    private User testUserOne;
-    private User testUserTwo;
-    private User testUserThree;
-
-    //@Before
     @Test
     public void compareUserTest(){
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        UserDao testDB = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build().getUserDao();
+        UserDao testDB = Room.inMemoryDatabaseBuilder(context,AppDatabase.class)
+                .build()
+                .getUserDao();
 
         //Test Users
-        User user = new User("Nacho","1234",12);
+        User user = new User(1, "Test","1234",12);
         testDB.insert(user);
 
         User user2 = testDB.getUserById(user.getId());
 
         assertEquals(user,user2);
 
-        user2.setUsername("Not Nacho");
+        user2.setUsername("Not Test");
         assertNotEquals(user,user2);
-
         testDB.insert(user2);
 
         User user3 = testDB.getUserById(user.getId());
         assertNotEquals(user,user3);
         assertEquals(user2,user3);
 
-//        mUserDao = mDb.us
-//        Log.i(TAG, "DB Up");
     }
 
+    //TODO: Decide if we need the @before and @after sections
 //    @After
 //    public void closeDb(){
 //        testDB.close();
@@ -62,8 +57,38 @@ public class testDb {
 //    }
 
     @Test
-    public void userInsertTest(){
+    public void userReplaceTest(){
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        UserDao testDB = Room.inMemoryDatabaseBuilder(context,AppDatabase.class)
+                .build()
+                .getUserDao();
 
+        User user = new User(1, "Test","1234",12);
+        testDB.insert(user);
+
+        String name = testDB.getUserById(1).getUsername();
+        assertEquals(user.getUsername(), name);
+
+        user.setUsername("New Test");
+        testDB.insert(user);
+        assertNotEquals(user.getUsername(), name);
+
+    }
+
+    @Test
+    public void userDeleteTest(){
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        UserDao testDB = Room.inMemoryDatabaseBuilder(context,AppDatabase.class)
+                .build()
+                .getUserDao();
+
+        User user = new User(1, "Test","1234",12);
+        testDB.insert(user);
+
+        assertNotNull(testDB.getUserById(1));
+
+        testDB.deleteUser(user);
+        assertNull(testDB.getUserById(1));
     }
 
     @Test //Check size of user list
