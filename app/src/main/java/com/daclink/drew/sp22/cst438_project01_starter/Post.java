ae2084c626c7d4e4f0dd6c007fb4e5e887396f68
Andrew Liddle <aliddle98@gmail.com>
2022-02-06 21:52:56 -0800

    Pre-merge API branch. Hitting 401 errors but should be stable for now.

---
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/Post.java
@@ -0,0 +1,31 @@
+package com.daclink.drew.sp22.cst438_project01_starter;
+
+public class Post {
+    // Testing variables and methods
+
+    private int userId;
+
+    private int id;
+
+    private String title;
+
+    private String body;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    // End testing
+}
