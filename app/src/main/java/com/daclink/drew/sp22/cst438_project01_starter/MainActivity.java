9e7536c44f60c3ad18944c0bda72647ec6582743
Ryan Trinh <ryanltrinh@gmail.com>
2022-02-10 16:36:56 -0800

    Updated Landing Page content_main.xml --> Uploaded Foodora Logos --> Created Username & Password Fields --> Created 'Login' Button --> Created 'Create Account' Button --> Added 'copywrite disclaimer' Needs: Layout Constraints and Buttons etc.

--- app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/MainActivity.java
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/MainActivity.java
@@ -23,28 +23,28 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    /* @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+         binding = ActivityMainBinding.inflate(getLayoutInflater());
+         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+         setSupportActionBar(binding.toolbar);
+         section commented out for UI
+         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
+         binding.fab.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                         .setAction("Action", null).show();
+             }
+         });
+     }
+ */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -67,10 +67,11 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+    //@Override
+    //public boolean onSupportNavigateUp() {
+       // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+       // return NavigationUI.navigateUp(navController, appBarConfiguration)
+      //          || super.onSupportNavigateUp();
+    //}
+
 }
