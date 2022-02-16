7eca2ab09c0d7214e681e321f1d050bef8a94c43
Bujangja <mulmlprpg@gmail.com>
2022-02-16 14:26:43 -0800

    Testing the log in button returning null even after assigning user object to user.

--- app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/MainActivity.java
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.daclink.drew.sp22.cst438_project01_starter;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.daclink.drew.sp22.cst438_project01_starter.Api.EdamamApi;
@@ -23,6 +24,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -33,50 +36,72 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-    UserDao mUserDAO;
-    User user = new User();
+    private UserDao userDao;
+    private User user;
 
-    Button createAccBtn;
-    Button loginBtn;
+    private Button createAccBtn;
+    private Button loginBtn;
 
-    EditText pass;
-    EditText name;
+    private String pass;
+    private String name;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-        setContentView(view);
-        mUserDAO = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.DB_NAME)
-                .allowMainThreadQueries()
-                .build()
-                .getUserDao();
+        setContentView(binding.getRoot());
+
+        getDatabase();
 
         createAccBtn = view.findViewById(R.id.create_acc);
-        name = view.findViewById(R.id.user);
-        pass = view.findViewById((R.id.pass));
+        name = view.findViewById(R.id.user).toString();
+        pass = view.findViewById((R.id.pass)).toString();
         loginBtn = view.findViewById(R.id.login);
 
     }
 
         //Button for logging in
-//    public void login(View v){
-//
-//        user.setUsername(name.getText().toString());
-//
-//    }
-/*
-* createAccount(View v)
-* Method for creating account and inputting user input information into user DB
-*
-*
-*/
-    public void createAccount(View v){
-        //For when the actual text boxes can be typed in
-         user.setUsername(name.getText().toString());
-         user.setPass(pass.getText().toString());
-         mUserDAO.insert(user);
+    public void login(View v){
+        if(verifyAccountLogin(name, pass)){
+            Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
+            startActivity(intent);
+        }
 
     }
+/**
+* createAccount(View v)
+* Takes you to new activity called createAccount when clicking on the createAccount button
+*/
+    public void createAccount(View v){
+
+        Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
+        startActivity(intent);
+
+    }
+
+    private boolean verifyAccountLogin(String username, String password){
+        user = userDao.getUserByUsername(username);
+
+        if (user.getUsername() != username){
+            Toast.makeText(this, "Incorrect account name or password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(user.getPass() != password){
+            Toast.makeText(this, "Incorrect account name or password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void getDatabase(){
+        userDao = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.DB_NAME)
+                .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
+                .build()
+                .getUserDao();
+    }
+
 }
+
